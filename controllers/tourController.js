8ca921af56d0ab7de0c0e33563368e34fdcf2ed9
mcallegari10 <martin.callegari94@gmail.com>
2022-01-08 20:49:27 -0300

    add validators and custom validator to the tour schema

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -37,8 +37,8 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Ivalid data sent'
-    })
+      message: err.message
+    });
   }
 }
 
--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,10 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
-    trim: true
+    trim: true,
+    maxLength: [40, 'A tour name must have less than 40 chars'],
+    minLength: [10, 'A tour name must have more than 10 chars']
+    // validate: [validator.isAlpha, 'Name must only contain characters']
   },
   slug: {
     type: String
@@ -21,11 +24,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have a group size']
+    required: [true, 'A tour must have a group size'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty can be: easy, medium or difficult'
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0']
   },
   ratingsQuantity: {
     type: Number,
@@ -35,7 +44,16 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price']
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        // This only points to current doc on NEW doc creation
+        return val < this.price;
+      },
+      message: 'Discount price ($VALUE) should be below the regular price'
+    }
+  },
   summary: {
     type: String,
     trim: true,
@@ -70,6 +88,7 @@ tourSchema.virtual('durationWeeks').get(function () {
 });
 
 // Document middleware
+// This only runs for .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
