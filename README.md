1c733f65a35b1b681b7ecb7d4399e35f17cc4719
mcallegari10 <martin.callegari94@gmail.com>
2022-02-11 17:00:43 -0300

    add security middlewares

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # NodeJS Training
 
 NodeJS training project with Express, MongoDB and Mongoose
+
+## Security best practices
+
+### Compromised database
+
+- Strongly encrypt passwords with salt and hash (bcrypt)
+- Strongly encrypt passwords reset tokens (sha256)
+
+### Brute force attacks
+
+- Use bcrypt (makes login requests slow)
+- Implement rate limiting (express-rate-limit)
+- Implement maximum login attempts
+
+### XSS attacks
+
+- Store JWT in HTTPOnly cookies
+- Sanitize user input data
+- Set special HTTP headers (helmet)
+
+### Denial of Service (DOS) attack
+
+- Implement rate limiting (express-rate-limit)
+- Limit body payload (body-parser)
+- Avoid evil regular expressions
+
+### NoSQL query injection
+
+- Use mongoose for MongoDB
+- Sanitize user input data
--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
@@ -8,13 +13,39 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-app.use(express.json());
+// GLOBAL MIDDLEWARES
+// Security headers
+app.use(helmet());
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// Limit http calls
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP'
+});
+app.use('/api', limiter);
+
+// Body parser
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL injection
+app.use(mongoSanitize());
+
+// Data sanitization for XSS
+app.use(xss());
+
+// Prevent parameter pollution
+// Have to whitelist params that can be passed multiple times
+app.use(hpp({
+  whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
+}));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, _, next) => {
   req.requestTime = new Date().toISOString();
   next();
